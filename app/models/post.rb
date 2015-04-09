f8ab23af91549e50b19cbea498ab5858f89af2bb
Abena Ofori <abenaboama@gmail.com>
2015-04-09 10:26:56 -0500

    post-markdown-assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,3 +19,12 @@ class Post < ActiveRecord::Base
     (@redcarpet.render body.to_s).html_safe
   end
 end
+
+  private
+  def render_as_markdown(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+end
