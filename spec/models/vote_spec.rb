20604232dab92037fe9f41394690d28ffa072762
Abena Ofori <abenaboama@gmail.com>
2015-04-23 22:00:43 -0400

    voting-checkpoint

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,17 +1,5 @@
-describe Vote do
-  describe "validations" do
-    describe "value validation" do
-      it "only allows -1 or 1 as values" do
-      	
-       v1 = Vote.new(value: 1)
-        expect(v.valid?).to eq(true)
+require 'rails_helper'
 
-        v2 = Vote.new(value: -1)
-        expect(v2.valid?).to eq(true)
-
-        v3 = Vote.new(value: 2)
-        expect(v3.valid?).to eq(false)
-      end
-    end
-  end
+RSpec.describe Vote, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
