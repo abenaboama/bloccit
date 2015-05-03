968010502726f45723ca15e93ca7254cdf700d4e
Abena Ofori <abenaboama@gmail.com>
2015-05-03 13:36:20 -0400

    Voting assignment

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,57 +1,57 @@
 class TopicsController < ApplicationController 
   def index
-     @topics = Topic.all
-     authorize @topics
+    @topics = Topic.all
+    authorize @topics
   end
 
   def new
-     @topic = Topic.new
-     authorize @topic
+    @topic = Topic.new
+    authorize @topic
   end
 
   def show
-     @topic = Topic.find(params[:id])
-        @posts = @topic.posts
-     authorize @topic
+    @topic = Topic.find(params[:id])
+    @posts = @topic.posts
+    authorize @topic
   end
 
   def edit
-     @topic = Topic.find(params[:id])
-     authorize @topic
+    @topic = Topic.find(params[:id])
+    authorize @topic
   end
  
-   def create
-     @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
-     authorize @topic
-     if @topic.save
-       redirect_to @topic, notice: "Topic was saved successfully."
-     else
-       flash[:error] = "Error creating topic. Please try again."
-       render :new
-     end
-   end
+  def create
+    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+    authorize @topic
+    if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash[:error] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
  
-   def update
-     @topic = Topic.find(params[:id])
-     authorize @topic
-     if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
-       redirect_to @topic
-     else
-       flash[:error] = "Error saving topic. Please try again."
-       render :edit
-     end
-   end
+  def update
+    @topic = Topic.find(params[:id])
+    authorize @topic
+    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+      redirect_to @topic
+    else
+      flash[:error] = "Error saving topic. Please try again."
+      render :edit
+    end
+  end
 
   def destroy
-     @topic = Topic.find(params[:id])
+    @topic = Topic.find(params[:id])
  
-     authorize @topic
-     if @topic.destroy
-       flash[:notice] = "Topic was deleted"
-       redirect_to topics_path
-     else
-       flash[:error] = "There was an error deleting the topic."
-       render :show
-     end
-   end
+    authorize @topic
+    if @topic.destroy
+      flash[:notice] = "Topic was deleted"
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting the topic."
+      render :show
+    end
+  end
 end
