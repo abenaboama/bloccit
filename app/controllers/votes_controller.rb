3e155db0d13a2e0841b5db5e69f1f649f4540494
Abena Ofori <abenaboama@gmail.com>
2015-05-03 15:18:07 -0400

    voting-checkpoint

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,23 +2,20 @@ class VotesController < ApplicationController
    before_action :load_post_and_vote
 
   def up_vote
+    update_vote!(1)
+    redirect_to :back
+  end
+  
+  def down_vote
+    update_vote!(-1)
+    redirect_to :back
+  end
+
+
+  def load_post_and_vote
     @post = Post.find(params[:post_id])
 
     @vote = @post.votes.where(user_id: current_user.id).first
-
-    if @vote
-      @vote.update_attribute(:value, 1)
-    else
-      @vote = current_user.votes.create(value: 1, post: @post)
-    end
-
-    # http://apidock.com/rails/ActionController/Base/redirect_to
-    redirect_to :back
-
-  private
-
-  def load_post_and_vote
-    # extract into this method the definition of @post and @vote
   end
 
   def update_vote!(new_value)
